12e90c121bd2a4766601eac15cce9b6e59151e63
Robert Huselius <klaatu@gort-tp.huseli.us>
2016-07-19 04:52:22 +0200

    Rensat lite och bytt namn på InfoWindow till Popup (heter så i Leaflet).

--- places/static/js/views/PlaceMarkerView.js
+++ places/static/js/views/PlaceMarkerView.js
@@ -2,8 +2,6 @@
  * Agerar både view åt en Place-modell och under-view åt MapView.
  * this.model : Place
  * Klassen instansieras i MapView::addMarker().
- * Events som kan triggas:
- * 	'remove' -> ta bort markören, lyssnas av MapView 
  */
 define([
 	'backbone',
@@ -12,21 +10,19 @@ define([
 	'utils',
 	'jquery',
 	'models/Place',
+	'view/MapView',
 ], function(Backbone, _, L, utils, $) {
 	var PlaceMarkerView = Backbone.View.extend({
 		events : {
 		},
 		markerEvents : {
-			'click' : 'openInfoWindow', 
+			'click' : 'openPopup', 
 		},
-		infoWindowEvents : {
-			'remove' : 'removeInfoWindow', 
+		popupEvents : {
+			'remove' : 'removePopup', 
 		},
-		infoWindowIsOpen : false,
+		popupIsOpen : false,
 
-		initialize : function() {
-			this.listenTo(this.model, 'remove', this.clear);
-		},	
 		// Innan denna körs måste this.mapview ha satts av MapView, annars baj
 		render : function() {
 			this.listenTo(this.mapview, 'filter', this.filter);
@@ -49,72 +45,60 @@ define([
 				this.showMarker();
 			}
 		},
-		/**
-		 * Ritar om markören. Körs när något i modellen ändrats (typiskt sett openNow).
-		 */
-		redraw : function() {
-			this.marker.setOptions(this.getMarkerOptions());
-		},
-		clear : function() {
-			this.trigger('remove');
-			this.remove();
-		},
 	    
 	    /**
-	     * INFOWINDOW
+	     * POPUP
 	     */
-	    openInfoWindow : function(e) {
-			if (!this.infoWindow) {
-				this.infoWindowTemplate = _.template($("#placeInfoWindowText").html());
-	   			this.infoWindow = new L.popup({
+	    openPopup : function(e) {
+			if (!this.popup) {
+				this.popupTemplate = _.template($("#placePopupText").html());
+	   			this.popup = new L.popup({
 	   				//maxWidth : "auto",
-	   				maxWidth : utils.infoWindowImageWidth + 40,
+	   				maxWidth : utils.popupImageWidth + 40,
 	   				autoClose : true,
 	   				autoPan : true,
 	   			}).setContent('Hämtar data ...');
-	    		this.bindInfoWindowEvents();
-	    		this.listenTo(this.model, 'change', this.updateInfoWindow);
+	    		this.bindPopupEvents();
+	    		this.listenTo(this.model, 'change', this.updatePopup);
 	    		this.model.fetch();
-		    	this.marker.bindPopup(this.infoWindow);
+		    	this.marker.bindPopup(this.popup);
 			}
-			this.infoWindowIsOpen = true;
+			this.popupIsOpen = true;
 	    },
-	    updateInfoWindow : function() {
+	    updatePopup : function() {
 			var $content = $("<div />");
-			$content.html(this.infoWindowTemplate(this.model.toJSON()));
+			$content.html(this.popupTemplate(this.model.toJSON()));
 			// När bilden laddats in i DOM måste popupen uppdateras
 			if (this.model.get('image') != '') {
-				this.$infoWindowImage = $content.find(".info-window-image");
-				this.$infoWindowImage.on('load', { that : this }, function(e) {
-					e.data.that.setInfoWindowImageWidth();
-					e.data.that.refreshInfoWindow();
+				this.$popupImage = $content.find(".info-window-image");
+				this.$popupImage.on('load', { that : this }, function(e) {
+					e.data.that.setPopupImageWidth();
+					e.data.that.refreshPopup();
 		    		e.data.that.marker.openPopup();
 				});
 				$(window).resize({ that : this }, function(e) {
-					e.data.that.setInfoWindowImageWidth();
-					e.data.that.refreshInfoWindow();
+					e.data.that.setPopupImageWidth();
+					e.data.that.refreshPopup();
 				});
 			} else {
 	    		this.marker.openPopup();
 			}
-			this.infoWindow.setContent($content[0]);
-			//this.refreshInfoWindow();
+			this.popup.setContent($content[0]);
 	    },
-		refreshInfoWindow : function() {
-			if (this.infoWindowIsOpen) {
-				this.infoWindow.update();
-//				this.marker.openPopup();
+		refreshPopup : function() {
+			if (this.popupIsOpen) {
+				this.popup.update();
 			}
 		},
-		setInfoWindowImageWidth : function() {
-			if (this.$infoWindowImage) {
-				this.$infoWindowImage.width(utils.infoWindowImageWidth);
-				this.$infoWindowImage.css("max-width", $(window).width() * 0.75);
+		setPopupImageWidth : function() {
+			if (this.$popupImage) {
+				this.$popupImage.width(utils.popupImageWidth);
+				this.$popupImage.css("max-width", $(window).width() * 0.75);
 			}
 		},
-		removeInfoWindow : function() {
-			delete this.$infoWindowImage;
-			this.infoWindowIsOpen = false;
+		removePopup : function() {
+			delete this.$popupImage;
+			this.popupIsOpen = false;
 		},
 
 	    /**
@@ -144,24 +128,24 @@ define([
 		},
 	    
 	    /**
-		 * Delegerar valda Leaflet-events till View-events med namn 'infoWindow:<leaflet-eventnamn>'.
-		 * Binder även explicit angivna lyssnare till Leaflet-events via this.infoWindowEvents.
+		 * Delegerar valda Leaflet-events till View-events med namn 'popup:<leaflet-eventnamn>'.
+		 * Binder även explicit angivna lyssnare till Leaflet-events via this.popupEvents.
 		 */
-		bindInfoWindowEvents : function() {
-			var infoWindowEventNames = ['add', 'remove', 'popupopen', 'popupclose'];
-			_.each(infoWindowEventNames, function(infoWindowEventName) {
+		bindPopupEvents : function() {
+			var popupEventNames = ['add', 'remove', 'popupopen', 'popupclose'];
+			_.each(popupEventNames, function(popupEventName) {
 				var handler = function() {
-					console.log('infoWindow:'+infoWindowEventName);
-					this.trigger('infoWindow:'+infoWindowEventName);
+					console.log('popup:'+popupEventName);
+					this.trigger('popup:'+popupEventName);
 				};
 				handler = _.bind(handler, this);
-				this.infoWindow.on(infoWindowEventName, handler);
+				this.popup.on(popupEventName, handler);
 			}, this);
-			_.each(this.infoWindowEvents, function(handler, event) {
+			_.each(this.popupEvents, function(handler, event) {
 				handler = _.isString(handler) ? this[handler] : handler;
 				if (_.isFunction(handler)) {
 					handler = _.bind(handler, this);
-					this.infoWindow.on(event, handler);
+					this.popup.on(event, handler);
 				}
 			}, this);
 		},
