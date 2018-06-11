978bf6eb6546a2b7241e791c3ebba92e87f8718f
Robert Huselius <robert@huseli.us>
2018-06-11 12:49:10 +0200

    Corrected static urls

--- budgethak/static/budgethak/js/app.js
+++ budgethak/static/budgethak/js/app.js
@@ -4,37 +4,37 @@ require.config({
 	paths : {
 		'underscore' : [
 			'//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
-			'../lib/underscore-min',
+			'../../lib/underscore-min',
 		],
 		'backbone' : [
 			'//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min',
-			'../lib/backbone-min',
+			'../../lib/backbone-min',
 		],
 		'jquery' : [
 			'//code.jquery.com/jquery-3.1.0.min',
-			'../lib/jquery-3.1.0.min',
+			'../../lib/jquery-3.1.0.min',
 		],
-		'jquery-ui' : '../lib/jquery-ui-1.12.0.custom/jquery-ui',
+		'jquery-ui' : '../../lib/jquery-ui-1.12.0.custom/jquery-ui',
 		'jquery-touchswipe' : [
 			'//cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.min',
-			'../lib/jquery.touchSwipe.min',
+			'../../lib/jquery.touchSwipe.min',
 		],
 		'leaflet' : [
 			'//cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0-rc.1/leaflet',
-			'../lib/leaflet/leaflet',
+			'../../lib/leaflet/leaflet',
 		],
 		'leaflet-markercluster' : [
 //			'//cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.0.0-rc.1.0/leaflet.markercluster',
 			'//cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.0.0-rc.1.0/leaflet.markercluster-src',
-			'../lib/leaflet.markercluster',
+			'../../lib/leaflet.markercluster',
 		],
 		'urljs' : [
 			'//cdnjs.cloudflare.com/ajax/libs/urljs/2.3.1/url.min',
-			'../lib/url.min',
+			'../../lib/url.min',
 		],
-		'leaflet-usermarker' : '../lib/leaflet-usermarker/leaflet.usermarker',
-		'router' : '../places/js/router',
-		'settings' : '../places/js/settings',
+		'leaflet-usermarker' : '../../lib/leaflet-usermarker/leaflet.usermarker',
+		'router' : 'router',
+		'settings' : 'settings',
 	},
 	shim : {
 		'underscore' : {
