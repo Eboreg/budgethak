8fa37e133a1b91f2954085d67993ea9b83bd3b05
Robert Huselius <robert@huseli.us>
2018-06-11 12:13:13 +0200

    Merging places app into budgethak

--- budgethak/apps.py
+++ budgethak/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class PlacesConfig(AppConfig):
-    name = 'places'
+class BudgethakConfig(AppConfig):
+    name = 'budgethak'
--- budgethak/urls.py
+++ budgethak/urls.py
@@ -18,14 +18,22 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.views.generic import TemplateView
+from rest_framework import routers
+from .views import IndexView, PlaceViewSet
+
+router = routers.DefaultRouter()
+router.register(r'^api/places', PlaceViewSet)
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [
     url(r'^tests$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
     url(r'^ajaximage/', include('ajaximage.urls')),
-    url(r'', include('places.urls')),
+    url(r'^place/(?P<id>.*)', IndexView.as_view(), name="place"),
+    url(r'^info$', IndexView.as_view(), name="info"),
+    url(r'^$', IndexView.as_view(), name="index"),
 ]
+urlpatterns += router.urls
 '''
 if settings.DEBUG:
     urlpatterns += patterns('django.contrib.staticfiles.views', url(r'^(?P<path>.*)$', 'serve'))
