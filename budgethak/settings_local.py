2723865b8cb1a0095fc72e68dd2bc76618818cbb
Robert Huselius <robert@huseli.us>
2018-07-27 12:22:29 +0200

    Changed ALLOWED_HOSTS

--- budgethak/settings_local.py
+++ budgethak/settings_local.py
@@ -2,4 +2,4 @@ from .settings import *
 
 HOSTNAME = 'http://127.0.0.1:8000'
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
+ALLOWED_HOSTS = ['huseli.us', 'budgethak.se', 'www.budgethak.se', '127.0.0.1', 'localhost', ]
